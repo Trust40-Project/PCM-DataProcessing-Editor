1a48f7dff25032f379bd56e2939ddb6deac0a2a7
Jakob <36604022+bakobjirner@users.noreply.github.com>
2018-11-07 13:39:53 +0100

    second upload of designer
    cleaned up the code

--- my.storeModel.design/src/my/storeModel/design/Services.java
+++ my.storeModel.design/src/my/storeModel/design/Services.java
@@ -1,7 +1,6 @@
 package my.storeModel.design;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import org.eclipse.emf.common.util.EList;
@@ -10,25 +9,22 @@ import org.eclipse.emf.ecore.EStructuralFeature;
 import org.modelversioning.emfprofile.Stereotype;
 import org.modelversioning.emfprofileapplication.StereotypeApplication;
 import org.palladiosimulator.mdsdprofiles.api.StereotypeAPI;
-import org.palladiosimulator.pcm.dataprocessing.dataprocessing.repository.Store;
+
 import org.palladiosimulator.pcm.dataprocessing.profile.api.ProfileConstants;
-import org.palladiosimulator.pcm.repository.BasicComponent;
 
 /**
  * The services class used by VSM.
  */
 public class Services {
+	
 
+
+
+	
+	
 	/**
-	 * See
-	 * http://help.eclipse.org/neon/index.jsp?topic=%2Forg.eclipse.sirius.doc%2Fdoc%2Findex.html&cp=24
-	 * for documentation on how to write service methods.
+	 * if the Component has the Stereotype StoreHaving, return the connected StoreContainer
 	 */
-	public EObject myService(EObject self, String arg) {
-		// TODO Auto-generated code
-		return self;
-	}
-
 	public EObject getStoreContainer(EObject component) {
 
 		if (!StereotypeAPI.getStereotypeApplications(component, ProfileConstants.STEREOTYPE_NAME_STORE_HAVING)
@@ -39,40 +35,36 @@ public class Services {
 			Stereotype stereotype = stereotypeApplication.getStereotype();
 			EStructuralFeature taggedValue = stereotype.getTaggedValue(ProfileConstants.TAGGED_VALUE_NAME_STORE_HAVING_CONTAINER);
 			EObject container = (EObject) stereotypeApplication.eGet(taggedValue);
+			
+		
 			return container;
 		}
 		return null;
 
 	}
 
+	
+	/**
+	 * return all the storeContainers of the BasicComponents in the current repository
+	 */
 	public List<EObject> getAllStoreContainers(EObject repo) {
 
 		EList<EObject> componentList = repo.eContents();
 		List<EObject> containers = new ArrayList<EObject>();
 
 		for (int i = 0; i < componentList.size(); i++) {
-			if (!StereotypeAPI
-					.getStereotypeApplications(componentList.get(i), ProfileConstants.STEREOTYPE_NAME_STORE_HAVING)
-					.isEmpty()) {
-				final EList<StereotypeApplication> pcmEntityStereotypeApplications = StereotypeAPI
-						.getStereotypeApplications(componentList.get(i), ProfileConstants.STEREOTYPE_NAME_STORE_HAVING);
-				final StereotypeApplication stereotypeApplication = pcmEntityStereotypeApplications.get(0);
-				final Stereotype stereotype = stereotypeApplication.getStereotype();
-				final EStructuralFeature taggedValue = stereotype.getTaggedValue(ProfileConstants.TAGGED_VALUE_NAME_STORE_HAVING_CONTAINER);
-				EObject container = (EObject) stereotypeApplication.eGet(taggedValue);
-				containers.add(container);
-			}
+			
+			EObject component = componentList.get(i);
+			
+			containers.add(getStoreContainer(component));
 		}
 
 		return containers;
 
 	}
 	
-	public EObject getComponent(EObject repo) {
-		
-		
-		
-		return repo.eContents().get(0);
-	}
-
+	
+	
+	
+	
 }
