19f79cfe1bdb5384897d48ec746f65b2160a8020
tobias <Tobias.Hassberg@web.de>
2019-11-20 17:05:20 +0100

    fixing broken odesign file

--- DataprocessingExtension/src/org/palladiosimulator/editors/sirius/repository/DataprocessingExtension/Services.java
+++ DataprocessingExtension/src/org/palladiosimulator/editors/sirius/repository/DataprocessingExtension/Services.java
@@ -1,7 +1,13 @@
 package org.palladiosimulator.editors.sirius.repository.DataprocessingExtension;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.palladiosimulator.mdsdprofiles.api.StereotypeAPI;
@@ -13,24 +19,37 @@ import org.palladiosimulator.pcm.dataprocessing.dataprocessing.repository.StoreC
 import org.palladiosimulator.pcm.dataprocessing.profile.api.ProfileConstants;
 import org.palladiosimulator.pcm.repository.BasicComponent;
 import org.palladiosimulator.pcm.repository.Interface;
+import org.palladiosimulator.pcm.repository.OperationInterface;
 import org.palladiosimulator.pcm.repository.OperationSignature;
+import org.palladiosimulator.pcm.repository.ProvidedRole;
 import org.palladiosimulator.pcm.repository.Repository;
+import org.palladiosimulator.pcm.repository.RequiredRole;
 
 
 /**
  * The services class used by VSM.
+ * @param <E>
  */
-public class Services {
-    
-    //String für dataSignature
+public class Services<E> {
+    public Collection<OperationInterface> getSetCorrespondingInterfaces(Repository repo){
+    	Collection<Interface> interfaceSet = repo.getInterfaces__Repository();
+    	Collection<OperationInterface> operationInterfaceSet = new ArrayList<>();
+    	
+    	for (Interface interface1 : interfaceSet) {
+    		if((interface1 instanceof OperationInterface)) {
+    			operationInterfaceSet.add((OperationInterface)interface1);
+    		}
+    			
+    	}
+    	return  operationInterfaceSet;
+    }
+	
+	
+	
     public String getDataRefinement(OperationSignature signature) {
     	OperationSignatureDataRefinement refinements = StereotypeAPI.getTaggedValue(signature, ProfileConstants.TAGGED_VALUE_NAME_OPERATION_SIGNATURE_DATA_REFINEMENT, ProfileConstants.STEREOTYPE_NAME_OPERATION_SIGNATURE_DATA_REFINEMENT);
-    	
-    	//System.out.println(refinements.toString());
     	Collection<ParameterBasedData> param = refinements.getParameterRefinements();
     	Collection<ResultBasedData> result = refinements.getResultRefinements();
-    	//String returnValue = refinements.getParameterRefinements().toString();
-    	//returnValue += refinements.getResultRefinements();
     	String returnValue ="";
     	for (ParameterBasedData parameterBasedData : param) {
 			returnValue += parameterBasedData.toString() + "\n";
@@ -38,23 +57,40 @@ public class Services {
     	for (ResultBasedData resultBasedData : result) {
 			returnValue += resultBasedData.toString() + "\n";
 		}
-    	
     	returnValue.trim();
-//    	String signatureaName = signature.getEntityName();
-    	
-    	//System.out.println("testOutput2"+ refinements.getClass());
     	return returnValue;
     }
     
     public Collection<ParameterBasedData> getParameterBasedData(OperationSignature signature) {
-    	OperationSignatureDataRefinement refinements = StereotypeAPI.getTaggedValue(signature, ProfileConstants.TAGGED_VALUE_NAME_OPERATION_SIGNATURE_DATA_REFINEMENT, ProfileConstants.STEREOTYPE_NAME_OPERATION_SIGNATURE_DATA_REFINEMENT);
+    	Set<OperationSignature> signatureSet = new HashSet<>();
+    	signatureSet.add(signature);
+    	if(StereotypeAPI.hasAppliedStereotype(signatureSet, ProfileConstants.STEREOTYPE_NAME_OPERATION_SIGNATURE_DATA_REFINEMENT)) {
+    		OperationSignatureDataRefinement refinements = StereotypeAPI.getTaggedValue(signature, ProfileConstants.TAGGED_VALUE_NAME_OPERATION_SIGNATURE_DATA_REFINEMENT, ProfileConstants.STEREOTYPE_NAME_OPERATION_SIGNATURE_DATA_REFINEMENT);
+    		return refinements.getParameterRefinements();
+    	}
     	
-    	//System.out.println(refinements.toString());
-    	return refinements.getParameterRefinements();
+    	//TODO warum ist hier ein elemeent ohne applieter stereotype??
+    	System.out.println(signature.getEntityName());
+    	return null;
+    }
+    
+
+    
+    public Collection<ResultBasedData> getReturnBasedData(OperationSignature signature) {
+    	Set<OperationSignature> signatureSet = new HashSet<>();
+    	signatureSet.add(signature);
+    	if(StereotypeAPI.hasAppliedStereotype(signatureSet, ProfileConstants.STEREOTYPE_NAME_OPERATION_SIGNATURE_DATA_REFINEMENT)) {
+    	    	OperationSignatureDataRefinement refinements = StereotypeAPI.getTaggedValue(signature, ProfileConstants.TAGGED_VALUE_NAME_OPERATION_SIGNATURE_DATA_REFINEMENT, ProfileConstants.STEREOTYPE_NAME_OPERATION_SIGNATURE_DATA_REFINEMENT);
+    	    	
+    	    
+    	    	
+    	    	return refinements.getResultRefinements();
+    	}
+    	System.out.println(signature.getEntityName());
+    	return null;
     }
     
     public String nameOfParameterBasedData(ParameterBasedData param) {
-    	System.out.println(param.getParameter().getParameterName());
     	return param.getParameter().getParameterName() + "  " + param.getEntityName();
     }
     
@@ -67,18 +103,26 @@ public class Services {
     }
     
     public String nameOfResultBasedData(ResultBasedData param) {
-    	return param.getEntityName();
+    	return "return " + param.getEntityName();
     }
     
     
     public Collection<Store> getStores(BasicComponent basicComponent) {
-    	StoreContainer obj2 = StereotypeAPI.getTaggedValue(basicComponent, ProfileConstants.TAGGED_VALUE_NAME_STORE_HAVING_CONTAINER, ProfileConstants.STEREOTYPE_NAME_STORE_HAVING);
-    	
-    	return obj2.getStores();
+    	Set<BasicComponent> basicComponentSet = new HashSet<>();
+    	basicComponentSet.add(basicComponent);
+    	if(StereotypeAPI.hasAppliedStereotype(basicComponentSet, ProfileConstants.STEREOTYPE_NAME_STORE_HAVING)) {
+    		return ((StoreContainer) StereotypeAPI.getTaggedValue(basicComponent, ProfileConstants.TAGGED_VALUE_NAME_STORE_HAVING_CONTAINER, ProfileConstants.STEREOTYPE_NAME_STORE_HAVING)).getStores();
+    	}
+    	return null;
     }
     
     public String getStoreName(Store store) {
     	return store.getEntityName();
     }
 
+    
+    public EObject getObj(EObject obj) {
+    	return obj;
+    }
+    
 }
