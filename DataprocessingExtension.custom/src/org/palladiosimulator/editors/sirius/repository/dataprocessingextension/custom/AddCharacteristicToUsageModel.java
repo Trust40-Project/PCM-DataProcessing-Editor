568498a5740763a5a80934346ebf1250990c7b92
tobias <Tobias.Hassberg@web.de>
2020-02-15 16:45:35 +0100

    characteristics in usage model

--- DataprocessingExtension.custom/src/org/palladiosimulator/editors/sirius/repository/dataprocessingextension/custom/AddCharacteristicToUsageModel.java
+++ DataprocessingExtension.custom/src/org/palladiosimulator/editors/sirius/repository/dataprocessingextension/custom/AddCharacteristicToUsageModel.java
@@ -7,12 +7,12 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.sirius.tools.api.ui.IExternalJavaAction;
 import org.palladiosimulator.editors.sirius.repository.dataprocessingextension.custom.service.Services;
 import org.palladiosimulator.mdsdprofiles.api.StereotypeAPI;
-import org.palladiosimulator.pcm.core.entity.Entity;
 import org.palladiosimulator.pcm.dataprocessing.dataprocessing.DataSpecification;
 import org.palladiosimulator.pcm.dataprocessing.profile.api.ProfileConstants;
 import org.palladiosimulator.pcm.resourceenvironment.ResourceEnvironment;
+import org.palladiosimulator.pcm.usagemodel.UsageModel;
 
-public class AddCharacteristicToResourceEnvironment implements IExternalJavaAction {
+public class AddCharacteristicToUsageModel implements IExternalJavaAction {
 
 	@Override
 	public boolean canExecute(Collection<? extends EObject> arg0) {
@@ -26,10 +26,9 @@ public class AddCharacteristicToResourceEnvironment implements IExternalJavaActi
 	@Override
 	public void execute(Collection<? extends EObject> arg0, Map<String, Object> arg1) {
 		EObject container = (EObject)arg1.get("container");
-		System.out.println(((Entity)container).getId());
-		ResourceEnvironment resEnv = Services.getParentOfType((EObject)arg1.get("container"), ResourceEnvironment.class);
+		System.out.println(container.getClass());
+		UsageModel resEnv = Services.getParentOfType((EObject)arg1.get("container"), UsageModel.class);
 		DataSpecification dataSpec = Services.getCorrespondingDataspecification(resEnv.eAllContents(), resEnv);
-		
 		Services.addCharacteristicToElement(dataSpec, container);
 		
 	}
--- DataprocessingExtension.custom/src/org/palladiosimulator/editors/sirius/repository/dataprocessingextension/custom/RemoveCharacteristicFromUsageModel.java
+++ DataprocessingExtension.custom/src/org/palladiosimulator/editors/sirius/repository/dataprocessingextension/custom/RemoveCharacteristicFromUsageModel.java
@@ -9,26 +9,24 @@ import org.eclipse.sirius.tools.api.ui.IExternalJavaAction;
 import org.palladiosimulator.editors.sirius.repository.dataprocessingextension.custom.service.Services;
 import org.palladiosimulator.pcm.dataprocessing.dataprocessing.characteristics.Characteristic;
 import org.palladiosimulator.pcm.resourceenvironment.ResourceEnvironment;
+import org.palladiosimulator.pcm.usagemodel.ScenarioBehaviour;
+import org.palladiosimulator.pcm.usagemodel.UsageModel;
+import org.palladiosimulator.pcm.usagemodel.UsageScenario;
 
-public class RemoveCharacteristicFromResourceEnvironment implements IExternalJavaAction {
-
+public class RemoveCharacteristicFromUsageModel implements IExternalJavaAction {
 	@Override
 	public boolean canExecute(Collection<? extends EObject> arg0) {
 		// TODO Auto-generated method stub
-		return true;
+		return false;
 	}
 
 	@Override
 	public void execute(Collection<? extends EObject> arg0, Map<String, Object> arg1) {
 		Characteristic characteristic = (Characteristic)arg1.get("element");
 		DSemanticDiagram semanticDiagram = Services.getParentOfType((EObject) arg1.get("containerView"), DSemanticDiagram.class);
-		ResourceEnvironment resInvironment = (ResourceEnvironment) semanticDiagram.getTarget();
-		
-		for (EObject eObject : resInvironment.getLinkingResources__ResourceEnvironment()) {
-			Services.removeCharacteristic(eObject, characteristic);
-		}
-		for (EObject eObject : resInvironment.getResourceContainer_ResourceEnvironment()) {
-			Services.removeCharacteristic(eObject, characteristic);			
+		UsageModel usageModel = (UsageModel) semanticDiagram.getTarget();
+		for (UsageScenario eObject : usageModel.getUsageScenario_UsageModel()) {
+				Services.removeCharacteristic(eObject.getScenarioBehaviour_UsageScenario(), characteristic);
 		}
 	}
 
