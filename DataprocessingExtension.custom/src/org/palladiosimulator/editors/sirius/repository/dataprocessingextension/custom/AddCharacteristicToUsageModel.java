96928459c5976669ee5e702e2a6dfa193d235d38
tobias <Tobias.Hassberg@web.de>
2020-02-17 12:12:49 +0100

    correctliy adding characteristic contianer to all scenariobehavior

--- DataprocessingExtension.custom/src/org/palladiosimulator/editors/sirius/repository/dataprocessingextension/custom/AddCharacteristicToUsageModel.java
+++ DataprocessingExtension.custom/src/org/palladiosimulator/editors/sirius/repository/dataprocessingextension/custom/AddCharacteristicToUsageModel.java
@@ -8,9 +8,12 @@ import org.eclipse.sirius.tools.api.ui.IExternalJavaAction;
 import org.palladiosimulator.editors.sirius.repository.dataprocessingextension.custom.service.Services;
 import org.palladiosimulator.mdsdprofiles.api.StereotypeAPI;
 import org.palladiosimulator.pcm.dataprocessing.dataprocessing.DataSpecification;
+import org.palladiosimulator.pcm.dataprocessing.dataprocessing.characteristics.CharacteristicContainer;
+import org.palladiosimulator.pcm.dataprocessing.dataprocessing.characteristics.impl.CharacteristicsFactoryImpl;
 import org.palladiosimulator.pcm.dataprocessing.profile.api.ProfileConstants;
 import org.palladiosimulator.pcm.resourceenvironment.ResourceEnvironment;
 import org.palladiosimulator.pcm.usagemodel.UsageModel;
+import org.palladiosimulator.pcm.usagemodel.UsageScenario;
 
 public class AddCharacteristicToUsageModel implements IExternalJavaAction {
 
@@ -26,11 +29,19 @@ public class AddCharacteristicToUsageModel implements IExternalJavaAction {
 	@Override
 	public void execute(Collection<? extends EObject> arg0, Map<String, Object> arg1) {
 		EObject container = (EObject)arg1.get("container");
-		System.out.println(container.getClass());
 		UsageModel resEnv = Services.getParentOfType((EObject)arg1.get("container"), UsageModel.class);
 		DataSpecification dataSpec = Services.getCorrespondingDataspecification(resEnv.eAllContents(), resEnv);
+		
+		if(StereotypeAPI.isStereotypeApplicable(container, ProfileConstants.STEREOTYPE_NAME_CHARACTERIZABLE)) {
+			CharacteristicContainer characContainer = CharacteristicsFactoryImpl.init().createCharacteristicContainer();
+			dataSpec.getCharacteristicContainer().add(characContainer);
+			UsageModel usageModel = Services.getParentOfType(container, UsageModel.class);
+			for (UsageScenario usageScenario : usageModel.getUsageScenario_UsageModel()) {
+				StereotypeAPI.applyStereotype(usageScenario.getScenarioBehaviour_UsageScenario(), ProfileConstants.STEREOTYPE_NAME_CHARACTERIZABLE);
+				StereotypeAPI.setTaggedValue(usageScenario.getScenarioBehaviour_UsageScenario(), characContainer, ProfileConstants.STEREOTYPE_NAME_CHARACTERIZABLE, ProfileConstants.TAGGED_VALUE_NAME_CHARACTERIZABLE_CONTAINER);
+			}
+		}	
 		Services.addCharacteristicToElement(dataSpec, container);
 		
 	}
-
 }
